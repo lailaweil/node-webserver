2a8380c7ddf49e30e897a563c217c7619e4eff40
lailaweil <lweil@est.frba.utn.edu.ar>
2019-02-14 12:00:58 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require ('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials'); // donde voy a tener mis partials
+app.set('view engine', 'hbs');
+
+//Log de requests de paginas
+app.use((req, res, next)=>{ 
+    var now = new Date().toString();
+    var log =  `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err)=>{ 
+        if(err){
+            console.log('unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// //Página en mantenimiento
+// app.use((req,res,next) =>{ //no deja que se rendeerie el codigo, no tengo next()
+//     res.render('maintance.hbs');
+// });
+
+//Cargo mis archivos estáticos
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => { //registro un helper (seria como una funcion)
+    return new Date().getFullYear(); 
+});
+
+//helper pasa a mayusculas
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+});
+
+//rendereo home page
+app.get('/', (req, res)=>{ 
+    res.render('home.hbs', {
+        pageTitle: 'Home',//template
+        welcomeMessage: 'Hello, welcome to my webpage!!'
+    });
+});
+
+//rendereo about page
+app.get('/about', (req,res)=>{ 
+    res.render('about.hbs',{
+        pageTitle: 'About pages',
+    });
+});
+
+//seteo el puerto
+app.listen(3000, ()=>{ 
+    console.log('Server is running');
+});
